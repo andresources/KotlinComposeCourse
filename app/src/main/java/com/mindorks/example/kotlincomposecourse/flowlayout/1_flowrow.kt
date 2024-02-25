24bef9eed7e537e6b34bfdc60e1094b5825cae6e
Krish <andresources1039@gmail.com>
2024-02-25 20:25:12 +0530

    Flow - Row

--- app/src/main/java/com/mindorks/example/kotlincomposecourse/flowlayout/1_flowrow.kt
+++ app/src/main/java/com/mindorks/example/kotlincomposecourse/flowlayout/1_flowrow.kt
@@ -5,17 +5,21 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -39,9 +43,12 @@ fun FlowRowDemo() {
     )
     {
         list.forEach {
-            Box(modifier = Modifier.clickable {
-                Toast.makeText(context,"$it",Toast.LENGTH_SHORT).show()
-            }
+            Box(modifier = Modifier
+                .clickable {
+                    Toast
+                        .makeText(context, "$it", Toast.LENGTH_SHORT)
+                        .show()
+                }
                 .padding(top = 5.dp)
                 .background(Color.LightGray, RoundedCornerShape(200.dp))) {
                 Text(modifier = Modifier.padding(top=5.dp,bottom=5.dp, start = 10.dp,end=10.dp), text = "$it")
@@ -53,21 +60,47 @@ fun FlowRowDemo() {
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
 fun Filters() {
+    var selList: MutableList<String> = mutableListOf()
     val filters = listOf(
         "Washer/Dryer", "Ramp access", "Garden", "Cats OK", "Dogs OK", "Smoke-free"
     )
-    FlowRow(
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        filters.forEach { title ->
-            var selected by remember { mutableStateOf(false) }
-            val leadingIcon: @Composable () -> Unit = { Icon(Icons.Default.Check, null) }
-            FilterChip(
-                selected,
-                onClick = { selected = !selected },
-                label = { Text(title) },
-                leadingIcon = if (selected) leadingIcon else null
-            )
+    Column() {
+
+        FlowRow(
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            filters.forEach { title ->
+                var selected by remember { mutableStateOf(false) }
+                val selectedItems: MutableState<List<String>> = mutableStateOf(listOf())
+                val leadingIcon: @Composable () -> Unit = { Icon(Icons.Default.Check, null) }
+                FilterChip(
+                    selected,
+                    onClick = {
+                        selected = !selected
+                        if (selected) {
+                            selList.add(title)
+                        } else {
+                            selList.remove(title)
+                        }
+
+                    }, colors = FilterChipDefaults.filterChipColors(
+                        containerColor = Color.Green,
+                        iconColor = Color.White,
+                        labelColor = Color.White,
+                        selectedContainerColor = Color.Cyan,
+                        selectedLabelColor = Color.Black,
+                        selectedLeadingIconColor = Color.Black
+                    ),
+                    label = { Text(title) }, shape = RoundedCornerShape(200.dp),
+                    leadingIcon = if (selected) leadingIcon else null
+                )
+            }
+        }
+        var context = LocalContext.current
+        Button(onClick = {
+            Toast.makeText(context.applicationContext,"${selList.size}",Toast.LENGTH_SHORT).show()
+        }) {
+            Text(text = "Subbmit")
         }
     }
 }
