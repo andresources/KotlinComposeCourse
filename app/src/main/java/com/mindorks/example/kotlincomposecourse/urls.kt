baef26d1377a0d372a721a0db47e39f4b9fa80f3
Krish <andresources1039@gmail.com>
2024-03-23 16:51:02 +0530

    Urls

---
+++ app/src/main/java/com/mindorks/example/kotlincomposecourse/urls.kt
@@ -0,0 +1,14 @@
+package com.mindorks.example.kotlincomposecourse
+
+//Step1 : https://developer.android.com/jetpack/compose/components?authuser=4
+//Step 2 : https://developer.android.com/jetpack/compose/designsystems?authuser=4
+//https://developer.android.com/jetpack/compose/graphics?authuser=4
+//https://developer.android.com/jetpack/compose/text/style-text?authuser=4
+//https://developer.android.com/jetpack/compose/designsystems?authuser=4
+//https://developer.android.com/jetpack/compose/layouts?authuser=4
+//https://developer.android.com/jetpack/compose/lifecycle?authuser=4
+//https://developer.android.com/jetpack/compose/kotlin?authuser=4
+//https://developer.android.com/jetpack/compose/animation/introduction?authuser=4
+//https://developer.android.com/jetpack/compose/performance/bestpractices?authuser=4
+//https://developer.android.com/jetpack/compose/touch-input/pointer-input?authuser=4
+//https://developer.android.com/jetpack/compose/text?authuser=4
