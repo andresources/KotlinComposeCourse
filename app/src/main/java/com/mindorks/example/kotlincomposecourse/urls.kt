40d0f4cb316e0a88efa81215023681fa5c3c2c9e
Krish <andresources1039@gmail.com>
2024-03-23 17:19:08 +0530

    Urls

--- app/src/main/java/com/mindorks/example/kotlincomposecourse/urls.kt
+++ app/src/main/java/com/mindorks/example/kotlincomposecourse/urls.kt
@@ -2,6 +2,7 @@ package com.mindorks.example.kotlincomposecourse
 
 //Step1 : https://developer.android.com/jetpack/compose/components?authuser=4
 //Step 2 : https://developer.android.com/jetpack/compose/designsystems?authuser=4
+//https://developer.android.com/jetpack/compose/resources?authuser=4
 //https://developer.android.com/jetpack/compose/graphics?authuser=4
 //https://developer.android.com/jetpack/compose/text/style-text?authuser=4
 //https://developer.android.com/jetpack/compose/designsystems?authuser=4
@@ -12,3 +13,9 @@ package com.mindorks.example.kotlincomposecourse
 //https://developer.android.com/jetpack/compose/performance/bestpractices?authuser=4
 //https://developer.android.com/jetpack/compose/touch-input/pointer-input?authuser=4
 //https://developer.android.com/jetpack/compose/text?authuser=4
+
+
+/*
+Layouts,Scaffold,App bars,Common buttons,Switch,Slider,Chip,Floating action buttons,Progress indicators,Card
+Dialog,Snackbars,Drawers,Lists and grids,Column,Row,Box,Lazy Column,Lazy Rows, Vertical Grid, Horizontal Grid
+ */
